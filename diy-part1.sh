cbc4641f485bfd6df0a2b62d0c88ffc892568da6
davidwu425 <jiawei31145@gmail.com>
2023-01-07 20:10:24 +0800

    Initial commit

---
+++ diy-part1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/Lancenas/Actions-Lean-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# fw876/helloworld
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+# Lienol/openwrt-package
+# sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
+# sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-package' feeds.conf.default
---
+++ diy-part2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#============================================================
+# https://github.com/Lancenas/Actions-Lean-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#============================================================
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
